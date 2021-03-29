d77e84f9e372e020c3af423774cfd30aefcc3b7c
shanestewart4 <shanestewartguitar@gmail.com>
2021-03-29 18:05:15 -0500

    bug fix

--- models/User.js
+++ models/User.js
@@ -40,7 +40,7 @@ userSchema.virtual('friendCount').get(function () {
     return this.friends.length;
 });
 
-const User = model('User', UserSchema);
+const User = model('User', userSchema);
 
 // module exports
 
